5bd96d285c31bcad7c6cd1376af7df4b36c76bce
Nicolás Cabello <cabellonic@gmail.com>
2022-04-19 15:10:17 -0300

    update: Improve variable naming

--- src/components/SingUp/SingUp.js
+++ src/components/SingUp/SingUp.js
@@ -2,18 +2,18 @@ import React, { useState } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
-import { initialValuesSingUpForm, formError } from './SingUpFormValues';
+import { initialValuesSingUpForm, formErrorMessages } from './SingUpFormValues';
 import styles from './SingUp.module.css';
 
 const singUpFormSchema = Yup.object().shape({
-  firstName: Yup.string().required(formError.firstNameRequired),
-  lastName: Yup.string().required(formError.lastNameRequired),
+  firstName: Yup.string().required(formErrorMessages.FIRST_NAME_REQUIRED),
+  lastName: Yup.string().required(formErrorMessages.LAST_NAME_REQUIRED),
   email: Yup.string()
-    .required(formError.emailRequired)
-    .email(formError.emailInvalid),
+    .required(formErrorMessages.EMAIL_REQUIRED)
+    .email(formErrorMessages.EMAIL_INVALID),
   password: Yup.string()
-    .required(formError.passwordRequired)
-    .min(6, formError.passwordMinLength),
+    .required(formErrorMessages.PASSWORD_REQUIRED)
+    .min(6, formErrorMessages.PASSWORD_MIN_LENGTH),
 });
 
 const SingUp = () => {
--- src/components/SingUp/SingUpFormValues.js
+++ src/components/SingUp/SingUpFormValues.js
@@ -5,11 +5,11 @@ export const initialValuesSingUpForm = {
   password: '',
 };
 
-export const formError = {
-  firstNameRequired: '¿Cómo te llamas?',
-  lastNameRequired: 'Necesitamos saber tu apellido',
-  emailRequired: 'Escribe tu correo por favor',
-  emailInvalid: 'El correo no es valido',
-  passwordRequired: 'Necesitas una contraseña para poder ingresar',
-  passwordMinLength: 'Tu contraseña debe tener al menos 6 caracteres',
+export const formErrorMessages = {
+  FIRST_NAME_REQUIRED: '¿Cómo te llamas?',
+  LAST_NAME_REQUIRED: 'Necesitamos saber tu apellido',
+  EMAIL_REQUIRED: 'Escribe tu correo por favor',
+  EMAIL_INVALID: 'El correo no es valido',
+  PASSWORD_REQUIRED: 'Necesitas una contraseña para poder ingresar',
+  PASSWORD_MIN_LENGTH: 'Tu contraseña debe tener al menos 6 caracteres',
 };
