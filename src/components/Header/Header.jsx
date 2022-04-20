6e52d46d35a770e785c3e538b9f4e66c20406653
Juanhernandezzs <juanmhdz99@gmail.com>
2022-04-20 16:00:58 -0300

    update: Use CSS modules instead of Sass

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Header.scss";
+import styles from "./Header.module.css";
 
 function Header() {
   return (
-    <nav className="navbar">
-      <div className="navbar-links">
-        <img className="navbar-img" src="https://i.ibb.co/7Qcvm6c/LOGO-SOMOS-MAS.png" alt="Logo" />
-        <Link className="navbar-link" to="/">Inicio</Link>
-        <Link className="navbar-link" to="/">Nosotros</Link>
-        <Link className="navbar-link" to="/">Actividades</Link>
-        <Link className="navbar-link" to="/">Novedades</Link>
-        <Link className="navbar-link" to="/">Testimonios</Link>
-        <Link className="navbar-link" to="/">Contacto</Link>
-        <Link className="navbar-link" to="/">Contribuye</Link>
+    <nav className={styles.navbar}>
+      <div className={styles.navlinks}>
+        <img className={styles.logo} src="https://i.ibb.co/7Qcvm6c/LOGO-SOMOS-MAS.png" alt="Logo" />
+        <Link className={styles.navlink} to="/">Inicio</Link>
+        <Link className={styles.navlink} to="/">Nosotros</Link>
+        <Link className={styles.navlink} to="/">Actividades</Link>
+        <Link className={styles.navlink} to="/">Novedades</Link>
+        <Link className={styles.navlink} to="/">Testimonios</Link>
+        <Link className={styles.navlink} to="/">Contacto</Link>
+        <Link className={styles.navlink} to="/">Contribuye</Link>
       </div>
-      <div className="navbar-buttons">
-        <button className="navbar-button loginbtn">Log in</button>
-        <button className="navbar-button registerbtn">Registrate</button>
+      <div className={styles.buttons}>
+        <button className={`${styles.button} ${styles.loginbtn}`}>Log in</button>
+        <button className={`${styles.button} ${styles.registerbtn}`}>Registrate</button>
       </div>
     </nav>
   );
