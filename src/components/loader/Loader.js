972d5c75be1789c612a6dcaebed6351dac2e9ce1
Facundo Villarreal <facu.nicolasvillarreal@gmail.com>
2022-04-19 23:27:49 -0300

    se agrego espacio entre imports y resto del codigo

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import loaderStyle from './style.module.css'
 import { TailSpin } from 'react-loader-spinner'
+
 export const Loader = () => {
     return (
         <div className={loaderStyle.container}>
