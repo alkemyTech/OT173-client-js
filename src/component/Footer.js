4ec503c775332fa63deb76cdeb3701970f96862e
Mariano Monfort <mmm.129k@gmail.com>
2022-04-20 09:59:11 -0300

    Corrección de los links
    Todos los links del footer y su respectivo css se pasaron al ingles.

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -5,33 +5,33 @@ import { LOGO_INFO, LINKS_SOCIAL } from "../data/data";
 export default function Footer() {
   return (
     <div className={style.container}>
-      <hr className={style.Rectangle_top} />
+      <hr className={style.rectangle_top} />
       <div className={style.footer_navigation}>
-        <p className={style.Noticias}>
-          <a href="/Noticias">Noticias</a>
+        <p className={style.information}>
+          <a href="/Information">Noticias</a>
         </p>
-        <p className={style.Actividades}>
-          <a href="/Actividades">Actividades</a>
+        <p className={style.activities}>
+          <a href="/Activities">Actividades</a>
         </p>
-        <p className={style.Novedades}>
-          <a href="/Novedades">Novedades</a>
+        <p className={style.news}>
+          <a href="/News">Novedades</a>
         </p>
-        <p className={style.Testimonios}>
-          <a href="/Testimonios">Testimonios</a>
+        <p className={style.testimonial}>
+          <a href="/Testimonial">Testimonios</a>
         </p>
-        <p className={style.Nosotros}>
-          <a href="/Nosotros">Nosotros</a>
+        <p className={style.us}>
+          <a href="/Us">Nosotros</a>
         </p>
-        <p className={style.Contacto}>
-          <a href="/Contacto">Contacto</a>
+        <p className={style.contact}>
+          <a href="/Contact">Contacto</a>
         </p>
       </div>
-      <div className={style.Sample_Logo}>
+      <div className={style.sample_logo}>
         {LOGO_INFO.map((item) => (
-          <img className={style.Logo_ONG} src={item.Image} alt={item.Text} />
+          <img className={style.logo_ong} src={item.Image} alt={item.Text} />
         ))}
       </div>
-      <hr className={style.Rectangle_Bottom} />
+      <hr className={style.rectangle_bottom} />
       <div className={style.icons}>
         {LINKS_SOCIAL.map((items) => (
           <div>
@@ -78,7 +78,7 @@ export default function Footer() {
           </div>
         ))}
       </div>
-      <span className={style.Info_Alkemy}>
+      <span className={style.info_alkemy}>
         2022 by Alkemy. All Rights Reserved.
       </span>
     </div>
