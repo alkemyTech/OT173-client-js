10df767265a82169d1f393a553f7cea27ad83f0a
Juanhernandezzs <juanmhdz99@gmail.com>
2022-04-21 04:32:56 -0300

    update: Header logo+menu items now passed as props

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,22 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 
-function Header() {
+function Header({ logo, menu }) {
   return (
     <nav className={styles.navbar}>
       <div className={styles.navlinks}>
-        <img className={styles.logo} src="https://i.ibb.co/7Qcvm6c/LOGO-SOMOS-MAS.png" alt="Logo" />
-        <Link className={styles.navlink} to="/">Inicio</Link>
-        <Link className={styles.navlink} to="/">Nosotros</Link>
-        <Link className={styles.navlink} to="/">Actividades</Link>
-        <Link className={styles.navlink} to="/">Novedades</Link>
-        <Link className={styles.navlink} to="/">Testimonios</Link>
-        <Link className={styles.navlink} to="/">Contacto</Link>
-        <Link className={styles.navlink} to="/">Contribuye</Link>
+        <img className={styles.logo} src={logo} alt="Logo" />
+        {menu.map((menuItem) => (
+          <Link className={styles.navlink} to={menuItem.link}>
+            {menuItem.name}
+          </Link>
+        ))}
       </div>
       <div className={styles.buttons}>
-        <button className={`${styles.button} ${styles.loginbtn}`}>Log in</button>
-        <button className={`${styles.button} ${styles.registerbtn}`}>Registrate</button>
+        <button className={`${styles.button} ${styles.loginbtn}`}>
+          Log in
+        </button>
+        <button className={`${styles.button} ${styles.registerbtn}`}>
+          Registrate
+        </button>
       </div>
     </nav>
   );
