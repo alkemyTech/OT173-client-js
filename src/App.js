0902907e2ca232d67f0f962a6a37c6d173d4ff6f
FedeMontenegro <fedejulianmontenegro@gmail.com>
2022-04-22 18:03:46 -0300

    Creación del componente Carousel

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router';
 import { Home } from './components/Home';
 import { News } from './components/News';
+import Carousel from './components/carousel/Carousel';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route to exact path='/' element={<Home/>}/>
         <Route to path='/news' element={<News/>}/>
       </Routes>
+      <Carousel />
     </div>
   );
 }
