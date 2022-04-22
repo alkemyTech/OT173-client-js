d0b752e53a69bbc72cb396e60d1cbe43b2dcf8c4
Facundo Villarreal <facu.nicolasvillarreal@gmail.com>
2022-04-22 13:22:13 -0300

    cambio de componente en el app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
 import { News } from './components/News';
-import { ContactScreen } from './components/contacto/ContactScreen';
+import { Home } from './components/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route to exact path='/' element={<ContactScreen />} />
+        <Route to exact path='/' element={<Home />} />
         <Route to path='/news' element={<News />} />
       </Routes>
     </div>
