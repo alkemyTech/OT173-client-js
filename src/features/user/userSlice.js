c51c76c64860ee0d353a4e65ba66edd61bc2ff52
Tomas Rodriguez <tomirodriguez1368@gmail.com>
2022-04-23 18:02:32 -0300

    updated: User slice setup

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,13 +1,73 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = {}
+
+export const fetchUser = createAsyncThunk(
+    "user/login",
+    async(dispatch,ApiThunk)=>{
+        try {
+            const userResponse = await axios.post("",dispatch)
+            return userResponse.data
+        } catch (error) {
+            return ApiThunk.rejectWithValue(error.response.data.message)
+        }
+    }
+)
+
+export const registerUser = createAsyncThunk(
+    "user/register",
+    async(dispatch,ApiThunk)=>{
+        try {
+            const userResponse = await axios.post("",dispatch)
+            return userResponse.data
+        } catch (error) {
+            return ApiThunk.rejectWithValue(error.response.data.message)
+        }
+    }
+)
+
+
+
+const initialState = {
+    currentUser: null,
+    isFetching: false,
+    error: false,
+}
 
 export const userSlice = createSlice({
     name:"user",
     initialState,
     reducers:{
-        login:(state,action)=>{
-            
+        clearState:(state)=>{
+            state.error = false
+            state.isFetching = false    
+        }
+    },
+    extraReducers:{
+        [fetchUser.pending] : (state)=>{
+            state.error = false
+            state.isFetching = true
+        },
+        [fetchUser.fulfilled] : (state,action)=>{
+            state.currentUser = action.payload
+            state.error = false
+            state.isFetching = false
+        },
+        [fetchUser.rejected] : (state)=>{
+            state.error = true
+            state.isFetching = false
+        },
+        [registerUser.pending]:(state)=>{
+            state.error = false
+            state.isFetching = true
+        },
+        [registerUser.fulfilled]:(state)=>{
+            state.error = false
+            state.isFetching = false
+        },
+        [registerUser.rejected] : (state)=>{
+            state.error = true
+            state.isFetching = false
         }
     }
 })
