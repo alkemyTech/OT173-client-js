4f04ded6f58276d20d3904d57ecace4902512adf
Nicolás Cabello <cabellonic@gmail.com>
2022-04-25 11:14:42 -0300

    update: Show the news createdAt date in a properly format

--- src/components/News/NewsCard.js
+++ src/components/News/NewsCard.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { DateUtils } from '../../util';
 import styles from './NewsCard.module.css';
 
 const NewsCard = ({ news }) => {
   const { name, image, createdAt, id } = news;
 
+  const date = DateUtils.formatDate(createdAt);
+
   return (
     <Link to={`/news/${id}`} className={styles.news_card}>
       <img className={styles.news_card_thumbnail} src={image} alt={name} />
       <h2 className={styles.news_card_title}>{name}</h2>
-      <p className={styles.news_card_date}>📆 {createdAt}</p>
+      <p className={styles.news_card_date}>📆 {date}</p>
     </Link>
   );
 };
