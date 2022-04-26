96ecc423000e219e6050994bdcf3c51fc1473b27
Facundo Villarreal <facundovillarreal@mangoodev.com>
2022-04-26 08:36:16 -0300

    mejora en la UI del formulario de contacto

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -22,55 +22,58 @@ export const ContactForm = () => {
         localStorage.setItem("consulta", JSON.stringify({ firstName, lastName, email, message }));
 
         reset();
-    }
+    };
 
     return (
-        <div className={contactFormStyles.container}>
-            <div>
-                <h4 className={contactFormStyles.formTitle}>Contacte con nosotros</h4>
-                <form onSubmit={handleSubmit}>
-                    <div className={contactFormStyles.formContainer}>
-                        <input
-                            className={contactFormStyles.inputFirstName}
-                            placeholder='Nombre'
-                            name='firstName'
-                            value={firstName}
-                            onChange={handleInputChange}
-                            autoComplete='off'
-                        />
-                        <input
-                            className={contactFormStyles.inputLastName}
-                            placeholder='Apellido'
-                            name='lastName'
-                            onChange={handleInputChange}
-                            value={lastName}
-                            autoComplete='off'
-                        />
-                        <input
-                            className={contactFormStyles.inputEmail}
-                            placeholder='Email'
-                            name='email'
-                            onChange={handleInputChange}
-                            value={email}
-                            autoComplete='off'
-                        />
-                        <textarea
-                            className={contactFormStyles.inputMessage}
-                            placeholder='Escribe tu consulta...'
-                            name='message'
-                            onChange={handleInputChange}
-                            value={message}
-                        />
-                    </div>
-                    <div className={contactFormStyles.containerBtnSend}>
-                        <button
-                            type='submit'
-                            className={contactFormStyles.buttonSend}
-                        >
-                            Enviar
-                        </button>
-                    </div>
-                </form>
+        <div className={contactFormStyles.content}>
+            <div className={contactFormStyles.container}>
+                <div className={contactFormStyles.formContainer}>
+                    <h4 className={contactFormStyles.formTitle}>Contacte con nosotros</h4>
+                    <hr className={contactFormStyles.styleOne} />
+                    <form onSubmit={handleSubmit}>
+                        <div>
+                            <input
+                                className={contactFormStyles.input}
+                                placeholder='Nombre'
+                                name='firstName'
+                                value={firstName}
+                                onChange={handleInputChange}
+                                autoComplete='off'
+                            />
+                            <input
+                                className={contactFormStyles.input}
+                                placeholder='Apellido'
+                                name='lastName'
+                                onChange={handleInputChange}
+                                value={lastName}
+                                autoComplete='off'
+                            />
+                            <input
+                                className={contactFormStyles.input}
+                                placeholder='Email'
+                                name='email'
+                                onChange={handleInputChange}
+                                value={email}
+                                autoComplete='off'
+                            />
+                            <textarea
+                                className={contactFormStyles.inputTextarea}
+                                placeholder='Escribe tu consulta...'
+                                name='message'
+                                onChange={handleInputChange}
+                                value={message}
+                            />
+                        </div>
+                        <div className={contactFormStyles.containerButton}>
+                            <button
+                                type='submit'
+                                className={contactFormStyles.buttonSend}
+                            >
+                                Enviar
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     )
