4ed825e1dfc5a9bce140a593f9c57ad477e9e96f
adrianadducchio <adrian.vhvhvh@gmail.com>
2022-04-27 19:20:51 -0300

    get news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { get } from './../services/apiService';
 
 export const News = () => {
+
+  useEffect(() => {
+    const getNews = async()=>{
+      const resp = await get('news')
+
+      console.log('reso', resp)
+    }
+
+    getNews()
+  }, [])
+  
+
+
+
   return (
     <div>News</div>
   )
