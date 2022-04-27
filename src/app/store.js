d4dd3763448ffbba8b18e873a144f05cccdcfcb0
Facundo Villarreal <facundovillarreal@mangoodev.com>
2022-04-27 01:34:13 -0300

    se agrego y se implemento redux-persist con el fin de persistir el usuario en el localStorage y en el store  a traves de redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,30 @@
+import { combineReducers } from 'redux';
+import storage from 'redux-persist/lib/storage';
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-import userReducer from '../features/user/userSlice'
+import userSlice from '../features/user/userSlice'
+import {
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+
+const persistConfig = {
+    key: 'user',
+    storage,
+};
+const reducers = combineReducers({ user: userSlice });
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export default configureStore({
-  reducer: {
-    counter: counterReducer,
-    user: userReducer
-  },
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 });
