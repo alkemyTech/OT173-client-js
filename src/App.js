ce715150e88cc2d603c6c467434cf30a20042690
Nicolás Cabello <cabellonic@gmail.com>
2022-04-28 20:38:53 -0300

    fix: Resolve merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
-import { LayoutBackOffice } from './layout/LayoutBackOffice';
 import { Home } from './components/Home';
+import { LayoutBackOffice } from './layout/LayoutBackOffice';
 import Login from './components/Login/Login';
 import News from './components/News/News';
 import './App.css';
