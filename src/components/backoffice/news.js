7166bce7f4a79feb6f6edcc5848949d7c244c086
Mariano Monfort <mmm.129k@gmail.com>
2022-04-30 13:36:38 -0300

    First Commit
    -Created component News
    -update dependencies.

---
+++ src/components/backoffice/news.js
@@ -0,0 +1,120 @@
+import React from "react";
+import { useState, useEffect, useMemo } from "react";
+import axios from "axios";
+import { useTable } from "react-table";
+
+
+export default function News() {
+    
+    const [values, setValues] = useState([])
+
+    const loadValues = async () =>{
+
+        const response = await axios.get('http://localhost:3000/news').catch(err => console.log(err));
+        
+        if(response){
+            const information = response.data;
+
+            setValues(information)
+        }
+    };
+
+
+const newsData = useMemo(() => [...values], [values]);
+
+const newsColumns = useMemo(
+    () =>
+    values[0]
+    ? Object.keys(values[0])
+    .filter((key) => key !== "rating")
+    .map((key) => {
+      return { Header: key, accessor: key };
+    })
+: [],
+[values]
+);
+
+const tableEdit = (hooks) => {
+    hooks.visibleColumns.push((columns) => [
+      ...columns,
+      {
+        id: "Edit",
+        Header: "Editar",
+        Cell: ({ row }) => (
+          <button >
+            Edit
+          </button>
+        ),
+      },
+    ]);
+  };
+
+  const tableDelete = (hooks) => {
+    hooks.visibleColumns.push((columns) => [
+      ...columns,
+      {
+        id: "Delete",
+        Header: "Borrar",
+        Cell: ({ row }) => (
+          <button >
+            Delete
+          </button>
+        ),
+      },
+    ]);
+  };
+
+  const buttonDelete = async (id) =>{
+      try {
+        const result = await axios.delete(`http://localhost:3000/news${id}`);
+        setValues(values.filter((val) => val.id !== id));
+        setValues(result)
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
+      
+const tableInstance = useTable({ columns: newsColumns, data: newsData },
+    tableDelete, tableEdit);
+
+const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance;
+    
+useEffect(() => {   
+    loadValues()
+},[]);
+
+return(
+    <>
+
+    <table {...getTableProps()}>
+    <thead>
+        {headerGroups.map((headerGroup) =>(
+            <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                    <th 
+                    {...column.getHeaderProps()}>{column.render("Header")}
+                     </th>
+                ))}
+                </tr>
+        ))}
+    </thead>
+    <tbody {...getTableBodyProps()} >
+            {rows.map((row) => {
+                prepareRow(row)
+                return (
+                    <tr {...row.getRowProps()}>
+                      {row.cells.map((cell) => (
+                        <td {...cell.getCellProps()}>
+                          {cell.render("Cell")}
+                        </td>
+                      ))}
+                    </tr>
+                  )
+                })}
+              </tbody>
+   </table>
+   </>
+);
+}
+
--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ import store from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import News from './components/backoffice/news';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <News />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
