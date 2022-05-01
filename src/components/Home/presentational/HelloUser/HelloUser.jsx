de18ebce41d416a931c72e0e63dbeb110c1dbce5
Tomas Rodriguez <tomirodriguez1368@gmail.com>
2022-05-01 18:54:23 -0300

    fix:delete Presentational Folder

--- src/components/Home/presentational/HelloUser/HelloUser.jsx
+++
@@ -1,8 +0,0 @@
-import HelloUserStyles from "./HelloUser.module.css"
-const HelloUser = () => {
-  return (
-    <h1 className={HelloUserStyles.hello}>Hello Roberto! Welcome Back! <span className={HelloUserStyles.hello_hand}>&#128400;</span></h1>
-  )
-}
-
-export default HelloUser
