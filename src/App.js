6eed212827b2df2f1ff8409e0da19b64d4cf3984
Nicolás Cabello <cabellonic@gmail.com>
2022-05-02 17:29:05 -0300

    update: Add Activity component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
+
 import { Home } from './components/Home';
 import { LayoutBackOffice } from './layout/LayoutBackOffice';
 import Login from './components/Login/Login';
 import News from './components/News/News';
+import Activity from './components/Activity/Activity';
 import './App.css';
 
 function App() {
@@ -13,6 +15,7 @@ function App() {
         <Route to exact path="/" element={<Home />} />
         <Route to path="/news" element={<News />} />
         <Route to path="/backoffice" element={<LayoutBackOffice />} />
+        <Route to path="/activities/:id" element={<Activity />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </div>
