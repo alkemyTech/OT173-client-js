5ea93eccb8903d0491e6c3804fa91893a449cb87
Mariano Monfort <mmm.129k@gmail.com>
2022-05-02 20:00:11 -0300

    Update App.js

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Route, Routes } from 'react-router';
+import { News } from './components/News';
+import { LayoutBackOffice } from './layout/LayoutBackOffice';
+import { Home } from './components/Home';
+import './App.css';
+import Login from './components/Login/Login';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route to exact path='/' element={<Home/>}/>
+        <Route to path='/news' element={<News/>}/>
+        <Route to path='/backoffice' element={<LayoutBackOffice/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
