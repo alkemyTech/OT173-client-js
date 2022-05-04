75fd107d93c1bca44268b3e79d88efb7b1e0df39
Tomas Rodriguez <tomirodriguez1368@gmail.com>
2022-05-04 00:06:24 -0300

    update: Default Username

--- src/components/HelloUser/HelloUser.jsx
+++ src/components/HelloUser/HelloUser.jsx
@@ -1,5 +1,5 @@
 import HelloUserStyles from "./HelloUser.module.css"
-const HelloUser = ({username}) => {
+const HelloUser = ({username="Guest"}) => {
   return (
     <h1 className={HelloUserStyles.hello}>Hello {username}! Welcome Back! <span className={HelloUserStyles.hello_hand}>&#128400;</span></h1>
   )
