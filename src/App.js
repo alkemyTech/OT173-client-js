80d0cf5d7261a1e200ab64e92a0d60c85f61d33a
adrianadducchio <adrian.vhvhvh@gmail.com>
2022-05-04 18:49:03 -0300

    resolved conflicts

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router';
 import { Home } from './components/Home';
 import { LayoutBackOffice } from './layout/LayoutBackOffice';
 import Login from './components/Login/Login';
+import { EditOrganizationForm } from './components/editOrganizationForm/EditOrganizationForm';
 import NewsDetail from './components/NewsDetail/NewsDetail';
 import SignUp from './components/SignUp/SignUp';
 import News from './components/News/News';
@@ -12,11 +13,12 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route to exact path="/" element={<Home />} />
-        <Route to path="/news" element={<News />} />
+        <Route to exact path='/' element={<Home/>}/>
+        <Route to path='/news' element={<News/>}/>
         <Route path='/news/:id' element={<NewsDetail/>}/>
-        <Route to path="/backoffice" element={<LayoutBackOffice />} />
-        <Route path="/login" element={<Login />} />
+        <Route to path='/backoffice' element={<LayoutBackOffice/>} />
+        <Route to path='/backoffice/edit-organization' element={<EditOrganizationForm/>}/>        
+        <Route path='/login' element={<Login/>}/>
         <Route path='/signup' element={<SignUp/>}/>
       </Routes>
     </div>
