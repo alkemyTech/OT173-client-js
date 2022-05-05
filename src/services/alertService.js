edec101ca0000c54b0f309104949702ed6e6f294
Facundo Villarreal <facundovillarreal@mangoodev.com>
2022-05-04 23:39:42 -0300

    eliminado espacios

--- src/services/alertService.js
+++ src/services/alertService.js
@@ -2,13 +2,13 @@ import swal from "sweetalert";
 
 export const confirm = (data) => {
     return (swal({ ...data, icon: "warning", buttons: ["No", "Yes"] })
-        .then(response =>
-            !response
-                ?
-                swal({ text: "Cancelled", icon: "info", buttons: false, timer: "1100" })
-                :
-                swal({ icon: "success", buttons: false, timer: "1100" })
-        ))
+                .then(response =>
+                    !response
+                    ?
+                    swal({ text: "Cancelled", icon: "info", buttons: false, timer: "1100" })
+                    :
+                    swal({ icon: "success", buttons: false, timer: "1100" })
+            ))
 }
 
 export const error = (data) => {
