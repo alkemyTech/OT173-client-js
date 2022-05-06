27301eca12cdab4cbcf6712b793d06c6bb308733
Juanhernandezzs <juanmhdz99@gmail.com>
2022-05-05 21:37:16 -0300

    update: Added routes to updated components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
-import { Home } from './components/Home';
+import Home from './components/Home/Home';
 import { LayoutBackOffice } from './layout/LayoutBackOffice';
 import Login from './components/Login/Login';
 import NewsDetail from './components/NewsDetail/NewsDetail';
 import SignUp from './components/SignUp/SignUp';
 import News from './components/News/News';
 import './App.css';
+import { ContactForm } from './components/contact/ContactForm';
+import EditHome from './components/EditHome/EditHome';
+import { EditUserForm } from './components/editUserForm/EditUserForm';
+import UserProfile from './components/Profile/UserProfile';
 
 function App() {
   return (
@@ -14,10 +18,14 @@ function App() {
       <Routes>
         <Route to exact path="/" element={<Home />} />
         <Route to path="/news" element={<News />} />
-        <Route path='/news/:id' element={<NewsDetail/>}/>
+        <Route path='/news/:id' element={<NewsDetail />} />
         <Route to path="/backoffice" element={<LayoutBackOffice />} />
         <Route path="/login" element={<Login />} />
-        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/contact' element={<ContactForm />} />
+        <Route path='/edithome' element={<EditHome />} />
+        <Route path='/edituser' element={<EditUserForm />} />
+        <Route path='/user' element={<UserProfile />} />
       </Routes>
     </div>
   );
