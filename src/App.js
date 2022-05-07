5ae90d1f58a94fa067d84f99ca02d2236f4ec501
adrianadducchio <adrian.vhvhvh@gmail.com>
2022-05-07 15:56:58 -0300

    apply animation transition

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router';
+import { Route, Routes, useLocation } from 'react-router';
 import Home from './components/Home/Home';
 import { LayoutBackOffice } from './layout/LayoutBackOffice';
 import Login from './components/Login/Login';
@@ -13,25 +13,32 @@ import { ContactForm } from './components/contact/ContactForm';
 import EditHome from './components/EditHome/EditHome';
 import { EditUserForm } from './components/editUserForm/EditUserForm';
 import UserProfile from './components/Profile/UserProfile';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 function App() {
+  const location = useLocation();
+
   return (
-    <div className="App">
-      <Routes>
-        <Route to exact path="/" element={<Home />} />
-        <Route to path="/news" element={<News />} />
-        <Route path="/news/:id" element={<NewsDetail />} />
-        <Route to path="/backoffice" element={<LayoutBackOffice />} />
-        <Route to path="/backoffice/news" element={<BackofficeNews />} />
-        <Route to path="/backoffice/users" element={<ListUsers />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/contact" element={<ContactForm />} />
-        <Route path="/edithome" element={<EditHome />} />
-        <Route path="/edituser" element={<EditUserForm />} />
-        <Route path="/user" element={<UserProfile />} />
-      </Routes>
-    </div>
+    <TransitionGroup component={null}>
+      <CSSTransition key={location.key} classNames="fade">
+        <div className="App">
+          <Routes>
+            <Route to exact path="/" element={<Home />} />
+            <Route to path="/news" element={<News />} />
+            <Route path="/news/:id" element={<NewsDetail />} />
+            <Route to path="/backoffice" element={<LayoutBackOffice />} />
+            <Route to path="/backoffice/news" element={<BackofficeNews />} />
+            <Route to path="/backoffice/users" element={<ListUsers />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/contact" element={<ContactForm />} />
+            <Route path="/edithome" element={<EditHome />} />
+            <Route path="/edituser" element={<EditUserForm />} />
+            <Route path="/user" element={<UserProfile />} />
+          </Routes>
+        </div>
+      </CSSTransition>
+    </TransitionGroup>
   );
 }
 
