961c11c71dadd106fbef81045048779bd11a9638
Facundo Villarreal <facundovillarreal@mangoodev.com>
2022-05-07 20:24:38 -0300

    se agrego mensaje de alerta en el deleteCategoryService

--- src/helpers/deleteCategory/deleteCategoryService.js
+++ src/helpers/deleteCategory/deleteCategoryService.js
@@ -1,5 +1,14 @@
+import { success, error as popUpError } from "../../services/alertService"
 import { remove } from "../../services/apiService"
 
-export const deleteCategory = async id => {
-    return await remove(`users/categories/${id}`)
+export const deleteCategory = async (id, setIsLoading) => {
+    setIsLoading(true);
+    const { ok, error } = await remove(`/categories/${id}`);
+    if (ok) {
+        success({ text: "Categoría eliminada correctamente" });
+        setIsLoading(false);
+    } else {
+        popUpError({ text: `${error.message}` });
+        setIsLoading(false);
+    }
 }
--- src/helpers/deleteCategory/showDeleteCategory.js
+++ src/helpers/deleteCategory/showDeleteCategory.js
@@ -1,20 +1,14 @@
-import { success, error as popUpError, willDelete } from '../../services/alertService';
+import { willDelete } from '../../services/alertService';
 import { deleteCategory } from './deleteCategoryService';
 
 export const showDeleteCategory = async (id, setIsLoading) => {
-    //setIsLoading se utiliza para detectar cuando utilizar el Loader y cuando re renderizar la vista de las categorías,
+        //setIsLoading se utiliza para detectar cuando utilizar el Loader y cuando re renderizar la vista de las categorías,
     //ej: en el componente donde se utilice este helper, debe crear un [isLoading, setIsLoading] = useState(false),
     //tambien utilizarlo en el useEffect para volver a renderizar la lista de categories.
-
     const response = await willDelete({ title: "¿Está seguro que desea eliminar esta categoría de forma permanente?" })
+   
     if (response) {
-        setIsLoading(true)
-        const { ok, error } = await deleteCategory(id)
-        ok
-            ?
-            success({ text: "Categoría eliminada correctamente" }) && setIsLoading(false)
-            :
-            popUpError({ text: `${error.message}` }) && setIsLoading(false)
+        await deleteCategory(id, setIsLoading);
     }
 }
 
