956f3a2e18935403aeffdbc4ea50360a1a84e0e3
Mariano Monfort <mmm.129k@gmail.com>
2022-05-07 13:41:53 -0300

    First Commit 62-Backoffice-menu
    -Se creo /layout/OutletLayout para renderizar los child routes para añadir proteccion de rutas mas adelante.
    -Se importaron los componentes y se acomodo el renderizado para separar que rutas quedaran publicas y cual privadas dentro de app.js.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router';
+import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import { LayoutBackOffice } from './layout/LayoutBackOffice';
 import Login from './components/Login/Login';
@@ -12,22 +12,31 @@ import { ContactForm } from './components/contact/ContactForm';
 import EditHome from './components/EditHome/EditHome';
 import { EditUserForm } from './components/editUserForm/EditUserForm';
 import UserProfile from './components/Profile/UserProfile';
+import OutletLayout from './layout/OutletLayout';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route to exact path="/" element={<Home />} />
-        <Route to path="/news" element={<News />} />
-        <Route path="/news/:id" element={<NewsDetail />} />
-        <Route to path="/backoffice" element={<LayoutBackOffice />} />
-        <Route to path="/backoffice/users" element={<ListUsers />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/contact" element={<ContactForm />} />
-        <Route path="/edithome" element={<EditHome />} />
-        <Route path="/edituser" element={<EditUserForm />} />
-        <Route path="/user" element={<UserProfile />} />
+            {/* Rutas publicas */}
+          <Route path="/" element={<OutletLayout />}>
+            <Route index element={<Home />} />
+            <Route path="news" element={<News />} />
+            <Route path="news/:id" element={<NewsDetail />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<SignUp />} />
+            <Route path="contact" element={<ContactForm />} />
+          </Route>
+            {/* Menu de opciones BackOffice Admin */}
+          <Route path="/backoffice" element={<OutletLayout />}>
+            <Route index element={<LayoutBackOffice />} />
+            <Route path="users" element={<ListUsers />} />
+            <Route path="edithome" element={<EditHome />} />
+            <Route path="edituser" element={<EditUserForm />} />
+            <Route path="user" element={<UserProfile />} />
+          </Route>
+            {/* Menu de opciones BackOffice User */}
+          <Route path="/backoffice/user" element={<UserProfile />} />
       </Routes>
     </div>
   );
