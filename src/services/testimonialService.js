c2255f22e5ede554cf9e286c49deb4fb3a4d7769
Facundo Villarreal <facundovillarreal@mangoodev.com>
2022-05-08 01:42:19 -0300

    se cambio de nombre al import success

--- src/services/testimonialService.js
+++ src/services/testimonialService.js
@@ -1,12 +1,12 @@
 import { patch, post } from "./apiService"
-import { succes, error as popUpError } from './alertService'
+import { success, error as popUpError } from './alertService'
 
 export const createTestimonial = async (formValue, navigate, setIsLoading, resetValues) => {
     setIsLoading(true)
     const { ok, error } = await post('/users/testimonials', formValue);
 
     if (ok) {
-        succes({text: "Categoría creada con éxito"});
+        success({text: "Categoría creada con éxito"});
         navigate('/testimonial');
         setIsLoading(false);
         resetValues();
@@ -21,7 +21,7 @@ export const updateTestimonial = async (id, formValue, setIsLoading, resetValues
     const { ok, error } = await patch(`users/testimonials/${id}`, formValue);
     
     if (ok) {
-        succes({text: "Categoría actualizada con éxito"});
+        success({text: "Categoría actualizada con éxito"});
         setIsLoading(false);
         resetValues();
     } else {
