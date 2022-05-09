d8896e0e62e9fc6c2089f19394e236d3dcb3ecdf
Nicolás Cabello <cabellonic@gmail.com>
2022-05-09 12:28:08 -0300

    update: Unifies styles of Login and SignUp components

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -7,62 +7,77 @@ import styles from './SignUp.module.css';
 import Header from '../Header/Header';
 
 const SingUp = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+
+  const signUpSubmit = async formvalue => {
+    const { ok, error } = await signUpRequest(formvalue);
+    if (ok) {
+      navigate('/');
+    } else {
+      popUpError({ text: `${error.message}` });
+    }
+  };
+
   const initialValues = {
     firstName: '',
     lastName: '',
     email: '',
     password: '',
   };
-  const signUpSubmit = async formvalue => {
-    const { ok, error } = await signUpRequest(formvalue)
-    if (ok) {
-      navigate("/")
-    } else {
-      popUpError({ text: `${error.message}` })
-    }
-  }
+
   return (
     <>
-      <Header logo={"/images/assets/logo1.png"} menu={[]} />
-      <section className={styles.sing_up}>
-        <h1 className={styles.title}>Formulario de registro</h1>
-        <Formik
-          initialValues={initialValues}
-          validationSchema={signUpFormValidationSchema}
-          onSubmit={signUpSubmit}
-        >
-          {({ isSubmitting }) => (
-            <Form className={styles.form}>
-              <Field type="text" name="firstName" placeholder="Nombre" />
-              <ErrorMessage name="firstName">
-                {error => <div className={styles.error}>{error}</div>}
-              </ErrorMessage>
+      <Header logo={'/images/assets/logo1.png'} menu={[]} />
+      <section className={styles.sign_up_wrapper}>
+        <div className={styles.sign_up}>
+          <h1 className={styles.title}>Registrarse</h1>
+          <div className={styles.image_container}>
+            <img
+              src="https://i.ibb.co/7Qcvm6c/LOGO-SOMOS-MAS.png"
+              alt="Registrarse"
+            />
+          </div>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={signUpFormValidationSchema}
+            onSubmit={signUpSubmit}
+          >
+            {({ isSubmitting }) => (
+              <Form className={styles.form}>
+                <Field type="text" name="firstName" placeholder="Nombre" />
+                <ErrorMessage name="firstName">
+                  {error => <div className={styles.error}>{error}</div>}
+                </ErrorMessage>
 
-              <Field type="text" name="lastName" placeholder="Apellido" />
-              <ErrorMessage name="lastName">
-                {error => <div className={styles.error}>{error}</div>}
-              </ErrorMessage>
+                <Field type="text" name="lastName" placeholder="Apellido" />
+                <ErrorMessage name="lastName">
+                  {error => <div className={styles.error}>{error}</div>}
+                </ErrorMessage>
 
-              <Field type="email" name="email" placeholder="Email" />
-              <ErrorMessage name="email">
-                {error => <div className={styles.error}>{error}</div>}
-              </ErrorMessage>
+                <Field type="email" name="email" placeholder="Email" />
+                <ErrorMessage name="email">
+                  {error => <div className={styles.error}>{error}</div>}
+                </ErrorMessage>
 
-              <Field type="password" name="password" placeholder="Contraseña" />
-              <ErrorMessage name="password">
-                {error => <div className={styles.error}>{error}</div>}
-              </ErrorMessage>
-              <button
-                type="submit"
-                className={styles.submit}
-                disabled={isSubmitting}
-              >
-                {isSubmitting ? 'Enviando...' : 'Registrarme'}
-              </button>
-            </Form>
-          )}
-        </Formik>
+                <Field
+                  type="password"
+                  name="password"
+                  placeholder="Contraseña"
+                />
+                <ErrorMessage name="password">
+                  {error => <div className={styles.error}>{error}</div>}
+                </ErrorMessage>
+                <button
+                  type="submit"
+                  className={styles.submit}
+                  disabled={isSubmitting}
+                >
+                  {isSubmitting ? 'Enviando...' : 'Registrarme'}
+                </button>
+              </Form>
+            )}
+          </Formik>
+        </div>
       </section>
     </>
   );
