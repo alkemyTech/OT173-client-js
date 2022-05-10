53cdeb7ed12cb85df540cc504191d8487ac621d4
Facundo Villarreal <facundovillarreal@mangoodev.com>
2022-05-10 13:31:42 -0300

    se añadio la funcion showDeleteCategory al service del mismo

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -1,14 +1,24 @@
-import { confirm } from '../../services/alertService';
-import { deleteCategory } from '../../services/deleteCategoryService';
+import { success, error as popUpError, confirm } from "./alertService"
+import { remove } from "./apiService"
 
 export const showDeleteCategory = async (id, setIsLoading) => {
         //setIsLoading se utiliza para detectar cuando utilizar el Loader y cuando re renderizar la vista de las categorías,
     //ej: en el componente donde se utilice este helper, debe crear un [isLoading, setIsLoading] = useState(false),
     //tambien utilizarlo en el useEffect para volver a renderizar la lista de categories.
     const response = await confirm({ title: "¿Está seguro que desea eliminar esta categoría de forma permanente?" })
-    console.log(response)
     if (response) {
         await deleteCategory(id, setIsLoading);
     }
 }
 
+const deleteCategory = async (id, setIsLoading) => {
+    setIsLoading(true);
+    const { ok, error } = await remove(`/categories/${id}`);
+    if (ok) {
+        success({ text: "Categoría eliminada correctamente" });
+        setIsLoading(false);
+    } else {
+        popUpError({ text: `${error.message}` });
+        setIsLoading(false);
+    }
+}
