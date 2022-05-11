22d1a9876bc5687f5dcd2d6314264137f64cbeae
Juanhernandezzs <juanmhdz99@gmail.com>
2022-05-10 21:19:12 -0300

    update: Redo of news component added delete button

---
+++ src/components/backoffice/NewsRow.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import styles from './News.module.css';
+import { Link } from 'react-router-dom';
+import { confirm } from '../../services/alertService';
+import { destroy } from '../../services/apiService';
+
+function NewsRow({ name, image, createdAt, id }) {
+  const handleDelete = async () => {
+    const confirmation = await confirm();
+    if (confirmation.success) {
+      const remove = function () {
+        destroy(`http://localhost:3001/news/${id}`);
+        window.location.reload();
+      };
+      setTimeout(remove, 1100);
+    }
+  };
+
+  return (
+    <tr>
+      <td className={styles.tabledata}>
+        <h2>{name}</h2>
+      </td>
+      <td className={styles.tabledata}>
+        <h2>{image}</h2>
+      </td>
+      <td className={styles.tabledata}>
+        <h2>{createdAt}</h2>
+      </td>
+      <td className={styles.tabledata}>
+        <div className={styles.actionbuttons}>
+          <Link to={`/editnews/${id}`}>
+            <button className={`${styles.button} ${styles.editbutton}`}>
+              Editar
+            </button>
+          </Link>
+          <button
+            className={`${styles.button} ${styles.deletebutton}`}
+            onClick={() => handleDelete()}
+          >
+            Eliminar
+          </button>
+        </div>
+      </td>
+    </tr>
+  );
+}
+
+export default NewsRow;
--- src/components/backoffice/news.js
+++ src/components/backoffice/news.js
@@ -1,90 +1,44 @@
-import React from 'react';
-import { useState, useEffect, useMemo } from 'react';
-import { useTable } from 'react-table';
+import React, { useEffect, useState } from 'react';
+import Header from '../Header/Header';
+import styles from './News.module.css';
+import NewsRow from './NewsRow';
 import { get } from '../../services/apiService';
 
-export default function BackofficeNews() {
-  const [values, setValues] = useState([]);
-  const newsData = useMemo(() => [...values], [values]);
-
-  const loadValues = async () => {
-    const response = await get('/news');
-    if (response) {
-      const information = response.data;
-      setValues(information);
-    }
-  };
-
-  const newsColumns = useMemo(
-    () =>
-      values[0]
-        ? Object.keys(values[0])
-            .filter(key => key !== 'id')
-            .map(key => {
-              return { Header: key, accessor: key };
-            })
-        : [],
-    [values]
-  );
-
-  const tableEdit = hooks => {
-    hooks.visibleColumns.push(columns => [
-      ...columns,
-      {
-        id: 'Edit',
-        Header: 'Editar',
-        Cell: ({ row }) => <button>Edit</button>,
-      },
-    ]);
-  };
-  const tableDelete = hooks => {
-    hooks.visibleColumns.push(columns => [
-      ...columns,
-      {
-        id: 'Delete',
-        Header: 'Borrar',
-        Cell: ({ row }) => <button>Delete</button>,
-      },
-    ]);
-  };
-
-  const tableInstance = useTable(
-    { columns: newsColumns, data: newsData },
-    tableDelete,
-    tableEdit
-  );
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    tableInstance;
+function BackofficeNews() {
+  const [news, setNews] = useState([]);
 
   useEffect(() => {
-    loadValues();
+    async function fetchData() {
+      const results = await get('http://localhost:3001/news');
+      setNews(results.data);
+    }
+    fetchData();
   }, []);
 
   return (
     <>
-      <table {...getTableProps()}>
-        <thead>
-          {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map(row => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map(cell => (
-                  <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                ))}
+      <Header logo={'/images/assets/logo1.png'} menu={[]} buttons={false} />
+      <div className={styles.container}>
+        <div className={styles.tablecontent}>
+          <table>
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Imagen</th>
+                <th>Fecha de creación</th>
+                <th className={styles.actions}>Acciones</th>
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
+            </thead>
+            <tbody>
+              {news.map(news => (
+                <NewsRow key={news.id} name={news.name} image={news.image} createdAt={news.createdAt} id={news.id} />
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </>
   );
 }
+
+export default BackofficeNews;
