55495139d4cade3b247a96f9d0ddf736d6cb8260
Juanhernandezzs <juanmhdz99@gmail.com>
2022-05-15 04:43:25 -0300

    fix: removed header import

--- src/components/backoffice/news.js
+++ src/components/backoffice/news.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import Header from '../Header/Header';
 import styles from './News.module.css';
 import NewsRow from './NewsRow';
 import { get } from '../../services/apiService';
