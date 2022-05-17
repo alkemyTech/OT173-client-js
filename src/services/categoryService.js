364665d794569d1d0d7e7220acdfe0a24b2dbc29
Facundo Villarreal <facundovillarreal@mangoodev.com>
2022-05-17 15:27:04 -0300

    se resolvieron conflictos y se corrigio el service de eliminar categoria

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -1,24 +1,14 @@
-import { success, error as popUpError, confirm } from "./alertService"
-import { remove } from "./apiService"
+import { success, error as popUpError, confirm } from "./alertService";
+import { destroy } from "./apiService";
 
-export const showDeleteCategory = async (id, setIsLoading) => {
-        //setIsLoading se utiliza para detectar cuando utilizar el Loader y cuando re renderizar la vista de las categorías,
-    //ej: en el componente donde se utilice este helper, debe crear un [isLoading, setIsLoading] = useState(false),
-    //tambien utilizarlo en el useEffect para volver a renderizar la lista de categories.
-    const response = await confirm({ title: "¿Está seguro que desea eliminar esta categoría de forma permanente?" })
-    if (response) {
-        await deleteCategory(id, setIsLoading);
-    }
-}
-
-const deleteCategory = async (id, setIsLoading) => {
-    setIsLoading(true);
-    const { ok, error } = await remove(`/categories/${id}`);
-    if (ok) {
-        success({ text: "Categoría eliminada correctamente" });
-        setIsLoading(false);
-    } else {
-        popUpError({ text: `${error.message}` });
-        setIsLoading(false);
-    }
+export const showDeleteCategory = async (id) => {
+    const response = await confirm({ title: "¿Está seguro que desea eliminar esta categoría de forma permanente?" });
+    if (response.success) {
+        const { ok, error } = await destroy(`/categories/${id}`);
+        if (ok) {
+            success({ text: "Categoría eliminada correctamente" });
+        } else {
+            popUpError({ text: `${error.message}` });
+        };
+    };
 }
