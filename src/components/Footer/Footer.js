92f4c761c568982322b2071a140c8ec2aa099db3
Mariano Monfort <mmm.129k@gmail.com>
2022-05-20 13:45:48 -0300

    First Commit
    -Se arreglo el logo que no se mostraba
    -Se arreglo los enlaces que se visualizaban repetidos
    -Se modifico el logo de LinkedIn para que visualmente sea similar a los demas.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,37 +1,37 @@
-import React from "react";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import style from "./Footer.module.css";
-import mailIcon from "../../data/icons/email.png";
-import facebookIcon from "../../data/icons/facebook.png";
-import instagramIcon from "../../data/icons/instagram.png";
-import phoneIcon from "../../data/icons/phone-call.png";
-import linkedinIcon from "../../data/icons/linke.png";
-
+import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import style from './Footer.module.css';
+import mailIcon from '../../data/icons/email.png';
+import facebookIcon from '../../data/icons/facebook.png';
+import instagramIcon from '../../data/icons/instagram.png';
+import phoneIcon from '../../data/icons/phone-call.png';
+import linkedinIcon from '../../data/icons/linkedin.png';
 
 export default function Footer() {
-  const [info, setInfo] = useState([])
+  const [info, setInfo] = useState([]);
 
-  useEffect(()=>{
-  
+  useEffect(() => {
     const fetchInfo = async () => {
-      const result = await axios.get('/organizations/public')
-      setInfo(result.data)
-  }
-  fetchInfo();
-}, []);
+      const result = await axios.get('/organizations/public');
+      setInfo(result.data);
+    };
+    fetchInfo();
+  }, []);
 
   return (
     <div className={style.container}>
-       <div className={style.sample_logo}>
-        {info.map((item) => (
-          <img className={style.logo_ong} src={item.image} alt="Logo ONG" />
-        ))}
+      <div className={style.sample_logo}>
+        <img
+          className={style.logo_ong}
+          src={'/images/assets/logo1.png'}
+          alt="Logo ONG"
+        />
       </div>
       <hr className={style.rectangle_top} />
       <div className={style.footer_navigation}>
         <p className={style.information}>
-          <a href="/information">Noticias</a>
+          <a href="/">Inicio</a>
         </p>
         <p className={style.activities}>
           <a href="/activities">Actividades</a>
@@ -51,61 +51,51 @@ export default function Footer() {
       </div>
       <hr className={style.rectangle_bottom} />
       <div className={style.icons}>
-        {info.map((item) => (
-          <div>
-            <a href={item.email} target="_blank" rel="noopener noreferrer">
-              <img
-                src={mailIcon}
-                className={style.icons_size}
-                alt="Icono Mail"
-              />
-            </a>
-            <a
-              href={item.instagram}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src={instagramIcon}
-                className={style.icons_size}
-                alt="Icono Instagram"
-              />
-            </a>
-            <a
-              href={item.facebook}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src={facebookIcon}
-                className={style.icons_size}
-                alt="Icono Facebook"
-              />
-            </a>
-            <a
-              href={item.linkedin}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src={linkedinIcon}
-                className={style.icons_size}
-                alt="Icono linkedin"
-              />
-            </a>
-            <a
-              href={item.phone}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src={phoneIcon}
-                className={style.icons_size}
-                alt="Icono Telefono"
-              />
-            </a>
-          </div>
-        ))}
+        {info
+          .filter(val => val.id === 8)
+          .map((item, index) => (
+            <div key={index}>
+              <a href={item.address} target="_blank" rel="noopener noreferrer">
+                <img
+                  src={mailIcon}
+                  className={style.icons_size}
+                  alt="Icono Mail"
+                />
+              </a>
+              <a
+                href={item.instagram}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={instagramIcon}
+                  className={style.icons_size}
+                  alt="Icono Instagram"
+                />
+              </a>
+              <a href={item.facebook} target="_blank" rel="noopener noreferrer">
+                <img
+                  src={facebookIcon}
+                  className={style.icons_size}
+                  alt="Icono Facebook"
+                />
+              </a>
+              <a href={item.linkedin} target="_blank" rel="noopener noreferrer">
+                <img
+                  src={linkedinIcon}
+                  className={style.icons_size}
+                  alt="Icono linkedin"
+                />
+              </a>
+              <a href={item.phone} target="_blank" rel="noopener noreferrer">
+                <img
+                  src={phoneIcon}
+                  className={style.icons_size}
+                  alt="Icono Telefono"
+                />
+              </a>
+            </div>
+          ))}
       </div>
       <span className={style.info_alkemy}>
         2022 by Alkemy. All Rights Reserved.
