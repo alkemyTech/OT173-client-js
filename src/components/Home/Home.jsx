81c51507394a0f021748affdabee5adcd064272c
Nicolás Cabello <cabellonic@gmail.com>
2022-05-23 19:07:38 -0300

    update: Remove duplicated Footer from home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,11 @@
 import HelloUser from '../HelloUser/HelloUser';
 import Carousel from '../carousel/Carousel';
-import Footer from '../Footer/Footer';
 
 const Home = () => {
   return (
     <>
       <Carousel />
       <HelloUser username={undefined} />
-      <Footer />
     </>
   );
 };
