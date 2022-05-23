b9d1562bdb922c9c229a0816791d39c525d04be2
Juanhernandezzs <juanmhdz99@gmail.com>
2022-05-23 12:05:33 -0300

    update: Page transition to secondary layout

--- src/layout/OutletLayoutSecondary.js
+++ src/layout/OutletLayoutSecondary.js
@@ -1,16 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header";
+import PageTransition from '../components/PageTransition/PageTransition';
 
 const OutletLayoutSecondary = () => {
 
     return (
-        <>
+        <PageTransition>
             <Header
                 logo={'/images/assets/logo1.png'}
                 menu={[]}
             />
             <Outlet />
-        </>
+        </PageTransition>
     )
 
 }
